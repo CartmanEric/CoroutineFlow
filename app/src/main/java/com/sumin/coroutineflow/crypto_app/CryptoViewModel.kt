716884c48f8f56c93b9662a8e081eae6f04a2e05
Eric <asd@sdf.ru>
2023-11-19 18:09:51 +0300

    description lesson5

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -12,8 +12,13 @@ class CryptoViewModel : ViewModel() {
     private val repository = CryptoRepository
 
     val state: LiveData<State> = repository.getCurrencyList()
+        // если .filter даст false то код дальше выполнятся не будет
         .filter { it.isNotEmpty() }
+        // можем мапать данные и приводить колекцию к стайту,
+        //.map кроме мапинга он сразу делает emit
         .map { State.Content(currencyList = it) as State }
+        // если после мапинга был не стайт Content то в .onStart мы его эмитим
         .onStart { emit(State.Loading) }
+        //приводим Flow к типу LiveData
         .asLiveData()
 }
