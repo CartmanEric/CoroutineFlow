ac709cc026ca2f09991108d9182d38be80870725
Eric <asd@sdf.ru>
2023-11-28 21:34:27 +0300

    description lesson13,0

--- app/src/main/java/com/sumin/coroutineflow/lesson13/Lesson13.kt
+++ app/src/main/java/com/sumin/coroutineflow/lesson13/Lesson13.kt
@@ -10,7 +10,11 @@ import kotlinx.coroutines.launch
 
 suspend fun main() {
     val scope = CoroutineScope(Dispatchers.Default)
-
+    //в данном примере если не использовать buffer() то поток будет ждать collect  и только
+    // после того как он получит его пойдет emit следуюшего значения
+    // указывае buffer() (по дефолту он идет на 64 обьекта) то он будет эмитить такое же
+    // количество элементов, и после переполнения будет ждать пока их обработают.
+    // у buffer() вторым параметром BufferOverflow.DROP_OLDEST BufferOverflow.DROP_LATEST
     val job = scope.launch {
         val flow: Flow<Int> = flow {
             repeat(10) {
